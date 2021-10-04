11c257deb0463e9bd823ab615c5d25356d14ca3e
munimrahman <munimrh@gmail.com>
2021-10-04 20:22:07 +0600

    404 page done

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notFoundImg from '../../images/404-not-found.png'
 
 const NotFound = () => {
     return (
-        <div>
-            <img src="https://i.stack.imgur.com/6M513.png" alt="404-not-found" height="600" />
+        <div className="container d-flex flex-column align-items-center mx-auto">
+            <h2 className="mb-3">404 - Nothing to See Here!</h2>
+            <div>
+                <Link to="/" className="btn btn-danger shadow-none">Go Home</Link>
+            </div>
+            <div className="w-50">
+                <img src={notFoundImg} alt="404-not-found" className="img-fluid" />
+            </div>
         </div>
     );
 };
